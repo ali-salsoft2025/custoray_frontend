eb86a9ad1586055229405a904b77c55fecdaab4f
ali-salsoft2025 <ali.hussain@salsoft.net>
2026-09-09 16:59:42 +0500

    Update GitHub Actions workflow for deployment. Rename job to 'deploy', adjust environment variable usage, and streamline steps for syncing files and installing dependencies. Enhance health check step for deployed services.

---
+++ ecosystem.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  apps: [
+    {
+      name: "custoray_frontend",
+      script: "./node_modules/next/dist/bin/next",
+      args: "start -p 3035",
+      cwd: __dirname,
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      max_memory_restart: "1G",
+      env: {
+        NODE_ENV: "production",
+        PORT: 3035,
+        NODE_OPTIONS: "--max-old-space-size=8192",
+      },
+    },
+  ],
+};
